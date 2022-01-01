c02f256e8a68138a7fb1363d768a33df6962df4f
André Antunes <afplays.new@gmail.com>
2022-01-01 22:28:15 +0000

    Version 1.1
    - Filter Invoice Items by it type;
    - Clear the code.

--- includes/hooks/Andrezzz_InvoiceWithIP.php
+++ includes/hooks/Andrezzz_InvoiceWithIP.php
@@ -1,6 +1,6 @@
 <?php
 /**
- *	Invoice with Primary/Dedicated IP WHMCS Hook version 1.0
+ *	Invoice with Primary/Dedicated IP WHMCS Hook version 1.1
  *
  *	@package     WHMCS
  *	@copyright   Andrezzz
@@ -16,16 +16,17 @@ use WHMCS\Database\Capsule;
 
 add_hook('InvoiceCreation', 1, function($vars) {
     global $_LANG;
+    $typeOfItems = array('Hosting', 'Setup', 'PromoHosting', 'Upgrade');
     $invoiceItems = Capsule::table('tblinvoiceitems')->where('invoiceid', $vars['invoiceid'])->get();
 
     foreach ($invoiceItems as $invoiceItem) {
         if ($invoiceItem->relid === 0) return;
+        if (!in_array($invoiceItem->type, $invoiceItem)) return;
         
         $dedicatedIP = Capsule::table('tblhosting')->where('id', $invoiceItem->relid)->value('dedicatedip');
-        if ($dedicatedIP === '') return;
 
         Capsule::table('tblinvoiceitems')->where('id', $invoiceItem->id)->update(array(
-            'description' => $invoiceItem->description . "\n" . $_LANG['primaryIP'] . ': ' . $dedicatedIP
+            'description' => $invoiceItem->description . "\n" . $_LANG['primaryIP'] . ': ' . trim($dedicatedIP)
         ));
     }
 });
